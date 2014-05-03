462aa5050201600ee03940e42817adca787db01c
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-03 14:06:50 -0400

    Initial setup
    app.js and primary view

---
+++ app.js
@@ -0,0 +1,24 @@
+/* initalize modules / db path */
+var express = require("express");
+var twilio = require("twilio");
+var _ = require('underscore');
+var client = new twilio.RestClient('AC645f23a47956757b6ee240ba83acc40d', 'da6895bd80208b87f5eeb94fa776961b');
+var app = express();
+
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('views/index.html');
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){
+	console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]);
+});
+
+
+/* node port config */
+var port = process.env.PORT || 5000;
+	app.listen(port, function() {
+	console.log("MEAN stack is running on port " + port);
+});
