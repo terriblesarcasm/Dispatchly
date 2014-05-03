c7fc13b4c21ec532390e5633dd7ff1464138e195
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-03 14:21:18 -0400

    Added basic bit.ly support

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ var _ = require('underscore');
 var client = new twilio.RestClient('AC645f23a47956757b6ee240ba83acc40d', 'da6895bd80208b87f5eeb94fa776961b');
 var app = express();
 
+/* bitly credentials / requirements */
+var BitlyAPI = require("node-bitlyapi");
+var Bitly = new BitlyAPI({
+    client_id: "149af734d55f5dea2049022bffeb8342ce40aa6d",
+    client_secret: "26fb726bafb6c08fa4e3fc4b89e7628601352326"    
+});
+
 /* serves main page */
+/* all other requests should fall below this */
 app.get("/", function(req, res) {
 	res.sendfile('views/index.html');
+});
+
+/* bitly API request */
+app.get("/api/bitly", function(req, res) {
+	Bitly.authenticate("spamr", "i001254m", function(err, access_token) {
+    // Returns an error if there was one, or an access_token if there wasn't 
+    Bitly.setAccessToken(access_token);
+    // Shorten the URL being passed through
+	    Bitly.shorten({longUrl:req.query.name}, function(err, results) {
+	    	res.send(results);
+			});
+	});
 });
 
 /* serves all the static files */
