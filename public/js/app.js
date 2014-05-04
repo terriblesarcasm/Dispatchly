e0d57cd466caec0405da4d4d89b3d337e16b8221
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-04 07:41:05 -0400

    Added AlertCtrl

--- public/js/app.js
+++ public/js/app.js
@@ -143,6 +143,11 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 })
 
 
+.controller('AlertCtrl', function ($scope, $window, $location, $q, $http) {
+
+})
+
+
 .controller('JoinGroupCtrl', function ($scope, $window, $location, $q, $http) {
 	$scope.joinGroup = function (group) {
 		$http.get('/db/join-group?group_id=' + group.group_id + '&password=' + group.password).success(function(response) {
