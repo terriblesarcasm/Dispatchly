7c83dac4161dc93f94986ea482b035c98f092e10
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-04 04:05:27 -0400

    Back to trying reverse

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
 var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.utils'])
 
-.filter('reverseAnything', function() {
+.filter('reverse', function() {
   return function(items) {
-    if(typeof items === 'undefined') { return; }
-    return angular.isArray(items) ? 
-      items.slice().reverse() : // If it is an array, split and reverse it
-      (items + '').split('').reverse().join(''); // else make it a string (if it isn't already), and reverse it
+    return items.slice().reverse();
   };
 })
 
@@ -41,7 +38,7 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 			var user = User.getUser();
 
 			return user;
-		})
+		});
 	}
 
 	return {
