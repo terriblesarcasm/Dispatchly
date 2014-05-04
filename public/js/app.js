e679cd6ad6a0a794ed7d61c92e66a41dffc33ec6
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-04 07:14:53 -0400

    Log sentGroup

--- public/js/app.js
+++ public/js/app.js
@@ -31,6 +31,7 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 	var group = {};
 	return {
 		setGroup: function(sentGroup) {
+			console.log('sentGroup is: ' + sentGroup);
 			group.name = sentGroup;
 		},
 		getGroup: function() {
