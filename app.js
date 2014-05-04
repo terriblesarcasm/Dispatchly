ddc18bdc16cfff242ecfa62a3ac496f5e1a0129c
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-04 08:47:53 -0400

    /api/twilio

--- app.js
+++ app.js
@@ -58,6 +58,37 @@ app.get("/", ensureAuthenticated, function(req, res) {
 	});
 });
 
+/* twilio API request */
+app.get("/api/twilio", createSMS);
+
+function createSMS(req, res, next) {
+	client.sms.messages.create({
+	    to:req.query.phone,
+	    from:req.query.from,
+	    body:req.query.bodymessage
+	}, function(error, message) {
+	    // The HTTP request to Twilio will run asynchronously. This callback
+	    // function will be called when a response is received from Twilio
+	    // The "error" variable will contain error information, if any.
+	    // If the request was successful, this value will be "falsy"
+	    if (!error) {
+	        // The second argument to the callback will contain the information
+	        // sent back by Twilio for the request. In this case, it is the
+	        // information about the text messsage you just sent:
+	        console.log('Success! The SID for this SMS message is:');
+	        console.log(message.sid);
+	        res.send('Success! The SID for this SMS message is: ' + message.sid);
+	 
+	        console.log('Message sent on:');
+	        console.log(message.dateCreated);
+	    } else {
+	        console.log('Oops! There was an error.');
+	        console.log(error);
+	    }
+	});
+}
+
+
 // app.get("/#/", ensureAuthenticated, function(req, res) {
 // 	res.sendfile('./views/app.html');
 // });
