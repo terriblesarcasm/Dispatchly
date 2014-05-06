2d236d4d7efbcd50f43c1c8300a2b5ad5236ba7b
terriblesarcasm <thiefness@gmail.com>
2014-05-05 22:22:19 -0400

    test url params

--- public/js/app.js
+++ public/js/app.js
@@ -74,7 +74,7 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 		controller: "SendAlertCtrl"
 	})
 	.state('confirmAlert', {
-		url: "/confirmAlert/{group, code}",
+		url: "/confirmAlert/{group}/{code}",
 
 		templateUrl: "public/partials/confirm-alert.temp",
 		controller: "ConfirmAlertCtrl"
