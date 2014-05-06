4990e808408f7a72ca15d0a74e3a224db1c52e98
terriblesarcasm <thiefness@gmail.com>
2014-05-06 08:33:20 -0400

    Fix form for login and change local passport strat

--- authentication.js
+++ authentication.js
@@ -34,10 +34,9 @@ passport.use('facebook-authz', new FacebookStrategy({
 
 passport.use(new LocalStrategy(
 	function(username, password, done) {
-		User.findOne({ username: username }, function(err, user) {
+		User.findOne({ username: username, password: password }, function(err, user) {
 			if (err) { return done(err);}
 			if (!user) { return done(null, false); }
-			if (!user.verifyPassword(password)) { return done(null, false); }
 			return done(null, user);
 		});
 	}
