259d226e0dc997746bcdb3d5efef2e22856f0b9d
Joe Barreca <jbarrec.tech@gmail.com>
2014-05-07 21:39:12 -0400

    group.name

--- public/js/app.js
+++ public/js/app.js
@@ -162,7 +162,7 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 		// create group
 	  	var URL = "https://dispatchninja.firebaseIO.com/groups/";
 	    $scope.groups = $firebase(new Firebase(URL));
-	    var new_group = $scope.groups.$child(group);
+	    var new_group = $scope.groups.$child(group.name);
 
 	}
 
