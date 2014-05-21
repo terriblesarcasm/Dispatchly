c3972967f624d48ed9b40a2bcb5742c6d7839829
terriblesarcasm <thiefness@gmail.com>
2014-05-21 00:39:07 -0400

    booyahkasha

--- public/js/app.js
+++ public/js/app.js
@@ -90,29 +90,6 @@ var app = angular.module('myApp', ['ui.router','ui.bootstrap','firebase', 'ui.ut
 })
 
 
-.filter('orderObjectBy', function(){
-  return function(input, attribute) {
-    if (!angular.isObject(input)) return input;
-
-    var array = [];
-    for(var objectKey in input) {
-      array.push(input[objectKey]);
-    }
-
-    function compare(a,b) {
-      if (a[attribute] < b[attribute])
-        return -1;
-      if (a[attribute] > b[attribute])
-        return 1;
-      return 0;
-    }
-
-    array.sort(compare);
-    return array;
-  }
-})
-
-
 .factory('User', function ($http) {
 	var user = {};
 
